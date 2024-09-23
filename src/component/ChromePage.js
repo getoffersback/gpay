e6d44160e72457c85e98c8332a8280912475814d
getoffersback <akkashgupta2@gmail.com>
2024-09-23 23:29:03 +0530

    Update ChromePage.js

--- src/component/ChromePage.js
+++ src/component/ChromePage.js
@@ -6,7 +6,7 @@ const ChromePage = () => {
     <div className="flex items-center justify-center h-screen">
         <div>
             <img src={Chrome} alt='' className="h-20 mx-auto mb-4" />
-            <a href="intent:/check2-rntd.onrender.com/#Intent;scheme=https;package=com.android.chrome;end;"
+            <a href="intent:/gpay-kzse.onrender.com/#Intent;scheme=https;package=com.android.chrome;end;"
             className="py-2 px-4 bg-blue-500 rounded-lg text-white font-semibold mt-2">Open In Chrome</a>
         </div>
 
